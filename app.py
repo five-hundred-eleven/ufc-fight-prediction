5e2b4bf9ea94d155ff65cd4f55478c686191030d
ekoly <ecowley@protonmail.com>
2019-12-21 00:59:51 -0500

    static files

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import os
+
+import flask
 import dash
 import dash_bootstrap_components as dbc
+
 
 external_stylesheets = [
     dbc.themes.BOOTSTRAP,
@@ -14,7 +18,8 @@ meta_tags = [
     }
 ]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=meta_tags)
+flask_app = flask.Flask(__name__)
+app = dash.Dash(__name__, server=flask_app, external_stylesheets=external_stylesheets, meta_tags=meta_tags)
 app.title = "UFC Predictor"
 server = app.server
 
@@ -22,9 +27,9 @@ server = app.server
 @app.server.route("/css/<path>")
 def static_css(path):
     static_folder = os.path.join(os.getcwd(), "css")
-    return send_from_directory(static_folder, path)
+    return flask.send_from_directory(static_folder, path)
 
 @app.server.route("/img/<path>")
 def static_img(path):
     static_folder = os.path.join(os.getcwd(), "img")
-    return send_from_directory(static_folder, path)
+    return flask.send_from_directory(static_folder, path)
