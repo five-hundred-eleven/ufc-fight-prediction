0a7911b7805e01bb43a1c3e05717f0cf50291635
ekoly <ecowley@protonmail.com>
2019-12-24 14:39:51 -0500

    fixes to fighter stats

--- service/fighter_service.py
+++ service/fighter_service.py
@@ -215,8 +215,8 @@ class FighterService:
             winner = blue_fighter
 
         shap_values = shap_values.sort_values()
-        pos_shaps = [self.__feature_to_name[s].strip() for s in shap_values.index[:3]]
-        neg_shaps = [self.__feature_to_name[s].strip() for s in shap_values.index[-3:]]
+        neg_shaps = [self.__feature_to_name[s].strip() for s in shap_values.index[:3]]
+        pos_shaps = [self.__feature_to_name[s].strip() for s in shap_values.index[-3:]]
 
         return winner_prob*100, winner, pos_shaps, neg_shaps
 
